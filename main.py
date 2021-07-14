a47dd7e1f177c0c4f4c2642e27216b33bcfde981
Qendrims <qs49351@universum-ks.org>
2021-07-14 15:45:32 +0200

    new

--- main.py
+++ main.py
@@ -1,45 +1,55 @@
 import random
+import wrong
 print("Welcome to Hangman game!")
 
 print("Type 'exit' to end the game, or press Enter to contine.")
 
 fin = open("./words.txt")
 words = fin.read().split()
-
-word = random.choice(words)
-length = len(word)
-
-print(f'Guess the word with {len(word)} letters')
-
-letters = []
-for i in range(length):
-    letters.append("_")
-
-print(" ".join(letters))
-
 while True:
+    word = random.choice(words)
+    length = len(word)
+
+    print(f'Guess the word with {len(word)} letters')
+
+    letters = []
+    for i in range(length):
+        letters.append("_")
+
+    print(" ".join(letters))
+    wrong_guesses = 0
+    while True:
 
 
-    guess = input("\nGuess a letter: ")
+        guess = input("\nGuess a letter: ")
 
-    if guess in word:
-        for i in range(length):
-            if guess == word[i]:
-                letters[i] = guess
+        if guess in word:
+            for i in range(length):
+                if guess == word[i]:
+                    letters[i] = guess
+
+
+        if guess not in word:
+            print(wrong.a[wrong_guesses])
+            wrong_guesses += 1
 
         print(" ".join(letters))
 
+        if "_" not in letters:
+            print("You won")
+            break
+
+        if wrong_guesses >= length:
+            print("You Lost")
+            break
+
+
+    close_page = input("Write 'exit' if you want to close the game or press 'ENTER' if you want to play again: ")
+    if close_page == "exit":
+        break
     else:
-        print("""
-        --------
-        |      |
-        |      
-        |
-        |
-        |
-       ---
-        """)
-    print(" ".join(letters))
+        pass
+
 
 
 
